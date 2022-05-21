082f9140a7bb6695a35b094e43243902e9f598ff
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-05-21 16:46:20 -0300

    final

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+
+const botao = document.querySelector("#botao");
+const tagH3 = document.querySelector("#situacao"); 
+const img = document.querySelector("#foto")
+
+botao.addEventListener("click", () =>{
+  if(botao.value == 'primeiro'){
+    situacao.innerText = 'Peco Alerta';
+    foto.src="./assets/img/pecoalerta.png";
+    botao.value = 'segundo';
+  }else if(botao.value == 'segundo'){
+    situacao.innerText = 'Peco Sono';
+    foto.src="./assets/img/pecosono.png";
+    botao.value = 'terceiro';
+  }else{
+    situacao.innerText = 'Peco Azedo';
+    foto.src="./assets/img/pecobravo.png";
+    botao.value = 'primeiro';
+  }
+});
+
+
+
+
+
+   
